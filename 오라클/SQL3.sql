54e16911b31637470c13ae5767a1abcd8c200ee0
hyeonjun-choi <garada123@naver.com>
2021-06-30 21:23:37 +0900

    20210630

--- 오라클/SQL3.sql
+++ 오라클/SQL3.sql
@@ -38,3 +38,140 @@ from
 commit;
 
 select lpad
+
+
+---
+update
+    employee
+set
+    salary = salary * 0.9
+where
+    emp_no in (
+        select
+        emp_no
+        from
+        (
+        select
+          rownum RNUM
+          , zxcvb.*
+        from (
+          select emp_no
+          from employee
+          order by
+          salary desc
+          , decode(jikup, '사장', 1, '부장', 2, '과장', 3, '대리', 4, '주임', 5, 6) asc
+          , hire_date asc
+          , decode(substr(jumin_num,7,1),'1','19','2','19','20')||substr(jumin_num,1,6) asc
+        ) zxcvb where rownum <=5 ) where RNUM >= 2
+    );
+
+
+
+update
+	employee
+set
+	salary = salary * 0.9
+where
+	emp_no in(
+		select emp_no from ( select rownum RNUM, zxcvb.* from (
+
+			select emp_no from employee
+			order by
+			salary desc
+			, decode(jikup, '사장', 1, '부장', 2, '과장', 3, '대리', 4) asc
+			, hire_date asc
+			, decode(substr(jumin_num,7,1), '1', '19', '2', '19', '20')||substr(jumin_num,1,6) asc
+
+		) zxcvb where rownum<=5 ) where RNUM>=2
+	);
+
+ update
+	employee
+set
+	salary = salary * 0.9
+where
+	emp_no in(
+		select emp_no from ( select rownum RNUM, zxcvb.* from (
+
+			select emp_no from employee
+			order by
+			salary desc
+			, decode(jikup, '사장', 1, '부장', 2, '과장', 3, '대리', 4) asc
+			, hire_date asc
+			, decode(substr(jumin_num,7,1), '1', '19', '2', '19', '20')||substr(jumin_num,1,6) asc
+
+		) zxcvb) where RNUM<=5 and RNUM>=2
+	);
+
+
+select
+	x.cnt-rownum+1					"번호", b.*
+from
+(
+	select
+		lpad( ' ', print_level*5, ' ' )||decode(print_no,0,'','ㄴ')||subject 	"글제목"
+		, writer							                                    "글쓴이명"
+		, to_char(reg_date, 'YYYY-MM-DD')			                        	"등록일"
+		, readcount					                                           	"조회수"
+	from
+		board
+	order by
+		group_no asc, print_no desc;
+) b, (select count(*) CNT from board) x
+
+
+select
+	(select count(*) from employee)-rownum+1	no_desc
+	, rownum					no_asc
+	, e.*
+from
+	employee e
+
+select
+	X.cnt-rownum+1				no_desc
+	, rownum					no_asc
+	, e.*
+from
+	employee e, (select count(*) from employee) X
+
+
+select
+	e.emp_name	"직원명"
+	, e.jikup		"직원직급"
+	, d.dep_name	"소속부서명"
+	, s.sal_grade_no	"연봉등급"
+from
+	employee e, dept d, salary_grade s
+where
+	e.dep_no=d.dep_no and (e.salary>=s.min_salary and e.salary<=s.max_salary)
+order by
+	s.sal_grade_no asc
+	, decode(e.jikup, '사장', 1, '부장', 2, '과장', 3, '대리', 4, '주임', 5, 6 ) asc
+	, case when substr( e.jumin_num,7,1) in ('1', '2') then '19' else '20' end
+	|| substr(e.jumin_num,1,6) asc;
+
+
+select
+	e.emp_name	"직원명"
+	, e.jikup		"직원직급"
+	, d.dep_name	"소속부서명"
+	, s.sal_grade_no	"연봉등급"
+from
+	(dept d inner join employee e on d.dep_no=e.dep_no) inner join
+	salary_grade s on (e.salary between s.min_salary and s.max_salary);
+order by
+	s.sal_grade_no asc
+	, decode(e.jikup, '사장', 1, '부장', 2, '과장', 3, '대리', 4, '주임', 5, 6 ) asc
+	, case when substr( e.jumin_num,7,1) in ('1', '2') then '19' else '20' end
+	|| substr(e.jumin_num,1,6) asc;
+
+--<96> 부하직원명, 부하직원직급, 직속상관명, 직속상관직급 출력<조건> 상관이 있는 직원만 포함
+select
+    e1.emp_name
+    , e1.jikup
+    , e2.emp_
+    , e2,jikup
+from
+    employee e1, employee e2
+where
+    e1.mgr_emp_no=e2.emp_no;
